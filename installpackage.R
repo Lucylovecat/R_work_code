4fb52339c673d8210c6c1a01cb4255558e1bf89f
Lucy <icdi3cat@gmail.com>
2022-04-01 09:43:21 +0800

    Update installpackage.R

--- installpackage.R
+++ installpackage.R
@@ -1,7 +1,9 @@
 # 套件管理
-install.packages(c("devtools", ""))
+install.packages(c("devtools"))
 # 繪圖工具
 install.packages(c("ggplot2", "RColorBrewer"))
 # 計算套件
 # comf-->計算PMV 
 install.packages(comf)
+# SQL套件
+install.packages(RMySQL)
